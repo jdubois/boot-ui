0efffe1ff6b044949f7c69915d8e52de814917da
Julien Dubois <julien.dubois@gmail.com>
2026-05-31 13:54:32 +0200

    Add some architecture issues for testing

---
+++ bootui-sample-app/src/main/java/io/github/jdubois/bootui/sample/ArchitectureIssuesController.java
@@ -0,0 +1,22 @@
+package io.github.jdubois.bootui.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/architecture")
+public class ArchitectureIssuesController {
+
+    // This should trigger ARCH-SPRING-001
+    @Autowired
+    // This should trigger ARCH-SPRING-002
+    private ProductRepository repository;
+
+    @RequestMapping("/some-errors")
+    public String errors() {
+        // This should trigger ARCH-CODE-001
+        System.out.println("This should trigger ARCH-CODE-001");
+        return "";
+    }
+}
